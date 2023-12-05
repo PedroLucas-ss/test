cdf5a23871b2aa690def63330a5aa179820567c6
Pedro Lucas <pedroluc1232@gmail.com>
2023-12-05 05:09:19 -0300

    prova.

--- app/src/main/java/com/example/tester/conta.java
+++ app/src/main/java/com/example/tester/conta.java
@@ -2,9 +2,14 @@ package com.example.tester;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import com.example.tester.TelaCadastroActivity.*;
+import com.example.tester.*;
+
 
 public class conta extends AppCompatActivity {
 
@@ -12,10 +17,30 @@ public class conta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conta);
+        EditText editTextUsername = findViewById(R.id.editTextUsername0);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextCRO = findViewById(R.id.editTextCRO);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText editTextPhone= findViewById(R.id.editTextTelefone);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText editTextEstado = findViewById(R.id.editTextEstado);
+
+
+
+        editTextUsername.setHint(Dentista.nomeDentista);
+        editTextUsername.setEnabled(false);
+        editTextEmail.setHint(Dentista.emailDentista);
+        editTextEmail.setEnabled(false);
+        editTextCRO.setHint(Dentista.CRO);
+        editTextCRO.setEnabled(false);
+        editTextPhone.setHint(Dentista.TelefoneDentista);
+        editTextPhone.setEnabled(false);
+        editTextEstado.setHint(Dentista.Estado);
+        editTextEstado.setEnabled(false);
+
     }
 
     public void voltarmenu(View view) {
         Intent in = new Intent( conta.this, MenuActivity.class);
         startActivity(in);
+
     }
 }
